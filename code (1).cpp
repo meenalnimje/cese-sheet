9c608e8b80591a4081cd55fb5c86f74f393e785a
meenal <meenalnimje@gmail.com>
2025-03-20 16:30:02 +0530

    ques name added

--- code (1).cpp
+++ code (1).cpp
@@ -1,34 +1,43 @@
+// ques 1 : weird algorithm
+
 #include <iostream>
 #include <algorithm>
-#include<unordered_map>
+#include <unordered_map>
 #include <numeric>
 #include <vector>
 using namespace std;
 unordered_map<int, vector<int>> dp;
-vector<int> solve(int n){
-    if(dp.find(n)!=dp.end()) return dp[n];
-    vector<int>ans;
+vector<int> solve(int n)
+{
+    if (dp.find(n) != dp.end())
+        return dp[n];
+    vector<int> ans;
     ans.push_back(n);
-    if(n==1){
-        return dp[n]=ans;
+    if (n == 1)
+    {
+        return dp[n] = ans;
     }
-    vector<int>next;
-    if(n%2==0){
-        next=solve(n/2);
+    vector<int> next;
+    if (n % 2 == 0)
+    {
+        next = solve(n / 2);
     }
-    else{
-        next=solve((n*3)+1);
+    else
+    {
+        next = solve((n * 3) + 1);
     }
-    ans.insert(ans.end(),next.begin(),next.end());
-    return dp[n]=ans;
+    ans.insert(ans.end(), next.begin(), next.end());
+    return dp[n] = ans;
 }
-int main() {
+int main()
+{
     int n;
-    cin>>n;
-    vector<int>ans=solve(n);
-    for(int i:ans){
-        cout<<i<<" ";
+    cin >> n;
+    vector<int> ans = solve(n);
+    for (int i : ans)
+    {
+        cout << i << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
--- code (4).cpp
+++ code (4).cpp
@@ -1,20 +1,23 @@
+// ques -2 : Missing  number
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
 using namespace std;
-int main() {
+int main()
+{
     long long n;
     cin >> n;
-    vector<long long>arr(n-1);
-    long long sum=0;
-    for(int i=0;i<n-1;i++){
-        cin>>arr[i];
-        sum+=arr[i];
+    vector<long long> arr(n - 1);
+    long long sum = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
     }
-    long long actualsum= (n*(n+1))/2;
-    long long ans=actualsum-sum;
-    cout<<ans<<endl;
-    
+    long long actualsum = (n * (n + 1)) / 2;
+    long long ans = actualsum - sum;
+    cout << ans << endl;
 
     return 0;
 }
